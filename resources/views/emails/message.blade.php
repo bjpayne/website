951df3d38f7c2ea97dd88337404a295d48496366
Ben <bj.payne@me.com>
2016-06-27 22:59:45 -0400

    Development Ben Working with email

--- resources/views/emails/message.blade.php
+++ resources/views/emails/message.blade.php
@@ -167,6 +167,7 @@
                             <p>From: {{ $name }}</p>
                             <p>Email: {{ $email }}</p>
                             <p>Message: {{ $text }}</p>
+                            <td></td>
                             <!-- button -->
                             <table class="btn-primary" cellpadding="0" cellspacing="0" border="0">
                                 <tr>
