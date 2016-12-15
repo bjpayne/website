9e85b4d1f3026f805f2f030c5d472e661af4957d
Ben <bj.payne@me.com>
2016-12-14 20:24:05 -0500

    Updates
    Update verbiage.
    
    Correct layout.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,12 @@
             <a href="https://northpointe.com">Northpointe Bank</a> in Grand Rapids, MI.
         </p>
         <p>
-            On a daily basis, I create dynamic web applications using PHP, MySql, Javascript, HTML and CSS. I enjoy
+            I create dynamic web applications using PHP, MySql, Javascript, HTML and CSS. I enjoy
             working with Laravel, Slim, WordPress, October CMS, SASS, Node, and jQuery.
         </p>
         <p>
-            I see every new experience as an opportunity to learn. My favorite projects are those that require me to
-            push beyond my boundaries and acquire new skills and knowledge in order to succeed.
+            I see every new challenge as an opportunity to learn. My favorite assignments are those that require me to
+            push beyond my boundaries and acquire new skills and knowledge in order to complete them.
         </p>
         <p class="last">
             You can get in touch with me anytime at <a href="mailto:benj@minpayne.com">benj@minpayne.com</a>.
